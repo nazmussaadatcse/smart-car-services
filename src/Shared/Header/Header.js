fd7251ba061cb65feda0a6ddd90348fae4577b10
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-03-28 15:27:38 +0600

    Dynamic Route and access route params

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,25 +1,48 @@
 import React from 'react';
+import { NavDropdown } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png'
 
 const Header = () => {
     return (
     
             <>
-                <Navbar bg="primary" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="#home">
-                            <img src={logo} height="30px" alt="" />
-                        </Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        </Nav>
-                    </Container>
-                </Navbar>
+
+ 
+    <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+            <img height={30} src={logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/about">
+                About
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link eventKey={2} as={Link} to ="/login">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
             </>
         );
