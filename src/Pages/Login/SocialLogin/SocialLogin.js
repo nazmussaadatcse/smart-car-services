5434bff1601e0601a4818f6a146d054d209d9d5d
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-04-01 03:44:10 +0600

    google login and error

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,20 +1,53 @@
 import React from 'react';
 import './SocialLogin.css'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+
+
+    // catch error 
+    if (error) {
+
+        errorElement = <div>
+                <p className='text-danger'>Error: {error.message}</p>
+            </div>
+
+    }
+    if(user){
+        navigate('/home');
+    }
+
     return (
         <div>
             <div className='d-flex align-items-center'>
                 <div className='line-color w-50 line-height'></div>
                 <p className='mt-2 px-2'>or</p>
                 <div className='line-color w-50 line-height'></div>
+                
             </div>
-            <button className='btn btn-primary mt-2 d-block mx-auto w-50'>
+
+            {/* show error for google auth */}
+            {errorElement}
+            {/* google signin  */}
+
+            <button onClick={()=>signInWithGoogle()} className='btn btn-primary mt-2 d-block mx-auto w-50'>
                 <img className='img' src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png" alt="" />
                 Google Sign In</button>
+
+
+            {/* Facebook Signin  */}
+
             <button className='btn btn-primary mt-2 d-block mx-auto w-50'>
                 <img className='img' src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-facebook-social-media-icon-png-image_6315968.png" alt="" />
                 Facebook Sign In</button>
+
+
+                {/* GitHub SignIn  */}
             <button className='btn btn-primary mt-2 d-block mx-auto w-50'>
                 <img className='img' src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" />
                 GitHub Sign In</button>
