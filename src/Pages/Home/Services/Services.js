23c3d93cd45224fca42c8f811330c4ac29d3d934
nazmussaadatcse <nazmus.bd97@gmail.com>
2023-08-18 03:58:09 +0600

    search bug fix

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,18 +3,22 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
-    const [isAsc,setIsAsc] = useState(true)
+    const [isAsc,setIsAsc] = useState(true);
     const [search,setSearch] =  useState('');
-    const searchRef = useRef();
+    const searchRef = useRef(null);
 
     useEffect(()=>{
-        fetch(`https://smart-car-server-snowy.vercel.app/services?search=${search}&order=${isAsc?'asc':'desc'}`)
+        console.log(search);
+        fetch(`https://smart-car-server-snowy.vercel.app/services?search=${search}&sort=${isAsc?'asc':'desc'}`)
         .then(res=>res.json())
         .then(data=> setServices(data))
+        console.log(search);
     },[isAsc,search])
+    
 
     const handleSearch = ()=>{
         setSearch(searchRef.current.value);
+        console.log(searchRef.current.value);
     }
 
     return (
@@ -23,8 +27,8 @@ const Services = () => {
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur repellat itaque iste voluptates, vel a? Alias pariatur temporibus repellendus exercitationem!</p>
-                <input ref={searchRef} type="search" className='input input-bordered input-sm' /><button onClick={handleSearch}>Search</button>
-                <button className='btn btn-ghost bg-red-100 ' onClick={()=>setIsAsc(!isAsc)}>{isAsc?'low-high':'high-low'}</button>
+                <input ref={searchRef} type="search" className='input input-bordered input-md p-4 m-2' /><button onClick={handleSearch} className='btn btn-primary'>Search</button>
+                <button className='mx-2 btn btn-ghost bg-red-100 ' onClick={()=>setIsAsc(!isAsc)}>{isAsc?'low-high':'high-low'}</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
